86687faed0ee87bf9217168471fc371004a5903b
Rupak Lama <rupaklamadev@gmail.com>
2024-01-07 17:47:00 -0500

    Refactor SQL queries for better readability and add new query for users with name ending in "n"

--- queries.sql
+++ queries.sql
@@ -1,33 +1,36 @@
 -- database: mysql
 
 -- Get all users from the users table
-
-SELECT * FROM users;
+SELECT *
+FROM users;
 
 -- Get all users from the users table with the name "John"
+SELECT *
+FROM users
+WHERE name = "John";
 
-SELECT * FROM users WHERE name = "John";
-
--- show all the tables in the database
-
+-- get all the tables in the database
 SHOW TABLES;
 
+
 -- get all the users from the users table ordered by the salary
+SELECT *
+FROM users
+ORDER BY salary;
 
-SELECT * FROM users ORDER BY salary;
-
---get all the users from the users table ordered by the salary in descending order
-
-SELECT * FROM users ORDER BY salary DESC;
+-- get all the users from the users table ordered by the salary in descending order
+SELECT *
+FROM users
+ORDER BY salary DESC;
 
 -- get all the users from the users table and join them with the addresses table
+select *
+from users join addresses on users.id = addresses.user_id;
 
-SELECT * FROM users JOIN addresses ON users.id = addresses.user_id;
+-- get all the users from the users table that have a name end with the litter "n";
+SELECT *
+FROM users
+WHERE name LIKE "%n";
 
--- get all the users from the users table that have a name that starts with the letter "B"
 
-SELECT * FROM users WHERE name LIKE "B%";
 
--- get all the users from the users table that have a name that ends with the letter "a"
-
-SELECT * FROM users WHERE name LIKE "%a";
